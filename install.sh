535bb80a3ff06189c6fce3b083b057e44958b31f
Plukkie <plukkie@gmail.com>
2020-07-09 10:36:34 +0200

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+################################################
+# This script will install required application
+# packages for the ZTD server.
+# 
+# Tested on:
+# - Ubuntu 20.04 LTS
+#
+# requirements
+# - A base Linux server with internet connectivity
+# - The server needs to be directly connected
+#   to the management network. This is the network
+#   where the network fabric switches have their
+#   management interfaces connected.
+# - run this installer as root
+################################################
+

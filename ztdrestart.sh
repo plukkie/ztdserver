b95d4b1357efa40bfd6770e68ad7dd7340bbc025
Plukkie <44134205+plukkie@users.noreply.github.com>
2025-02-13 11:33:57 +0100

    Update ztdrestart.sh

--- ztdrestart.sh
+++ ztdrestart.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-containerid_jenkins=`docker ps -a | grep jenkins-blueocean | awk '{print $1}'`
-containerid_ztdserver=`docker ps -a | grep ztd.*httpd | awk '{print $1}'`
+containerid_jenkins=`docker ps -a | grep jenkins | awk '{print $1}'`
 ztdefault_network='ztdserver_default'
 
 docker network disconnect $ztdefault_network $containerid_jenkins
 
 cd /home/peter/ztdserver
 docker-compose down && docker-compose up -d
-docker start $containerid_jenkins
+#docker start $containerid_jenkins
 
+containerid_httpd=`docker ps -a | grep ztd.*httpd | awk '{print $1}'`
 docker network connect $ztdefault_network $containerid_jenkins
-docker network connect jenkins $containerid_ztdserver
+docker network connect jenkins $containerid_httpd
+
 
